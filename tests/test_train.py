7074ca9d79d1b8d2aa35d6a48b4fd282097007d8
mmorelli1656 <morellimichele1997@gmail.com>
2026-06-25 11:53:32 +0200

    feat: add tests, gitignore and init files

---
+++ tests/test_train.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 11:37:17 2026
+
+@author: WKS
+"""
+
+from src.train import train
+
+def test_accuracy():
+    _, acc = train()
+    assert acc > 0.9
