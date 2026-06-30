f69a5328ffe688d11019604480cccc0d3ce47d98
mmorelli1656 <morellimichele1997@gmail.com>
2026-06-30 15:42:04 +0200

    test: ci/cd test

--- tests/test_train.py
+++ tests/test_train.py
@@ -7,10 +7,10 @@ Created on Thu Jun 25 11:37:17 2026
 
 from src.train import train
 
-# def test_accuracy():
-#     _, acc = train()
-#     assert acc > 0.9
-    
 def test_accuracy():
     _, acc = train()
-    assert acc > 1.5
+    assert acc > 0.9
+    
+# def test_accuracy():
+#     _, acc = train()
+#     assert acc > 1.5
